5ffad91bfd1b3a16288cee907671c4975729b1a8
elkcl <28754537+elkcl@users.noreply.github.com>
2024-09-02 22:21:32 +0300

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{pkgs ? import <nixpkgs> {}}: let
+  rev = "ee355d50a38e489e722fcbc7a7e6e45f7c74ce95";
+  pinned = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "18wixs4xvnsnfsj48d29ryvvh2jdqadqnnd05kvjkqdmgmllqbky";
+  }) {};
+
+  postgresql = pinned.postgresql_11;
+in
+  pkgs.mkShell {
+    nativeBuildInputs = [postgresql pkgs.pgadmin4];
+  }
+
